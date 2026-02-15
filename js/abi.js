952606f616e03e1abe333ab41a40be9ad7a39bdb
Pipin Antosin <77954264+pipinantosin@users.noreply.github.com>
2026-02-15 15:34:16 +0700

    push zip

---
+++ js/abi.js
@@ -0,0 +1,11 @@
+const POOL_ABI = [
+    "function slot0() view returns (uint160 sqrtPriceX96, int24 tick, uint16, uint16, uint16, uint8, bool)",
+    "function liquidity() view returns (uint128)",
+    "function token0() view returns (address)",
+    "function token1() view returns (address)"
+];
+
+const ERC20_ABI = [
+    "function decimals() view returns (uint8)",
+    "function symbol() view returns (string)"
+];
