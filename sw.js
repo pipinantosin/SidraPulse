a7f98c9545281716884ba3d859c00de9f02d0a88
Pipin Antosin <77954264+pipinantosin@users.noreply.github.com>
2026-02-24 15:13:24 +0700

    Update sw.js via push-zip tool

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "sidrapulse-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/js/balance.js",
+  "/js/tokens.js",
+  "/sidrapulse.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch (Network First Strategy)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
