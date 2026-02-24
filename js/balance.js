9314bc6a322d8e6a36c6258ede928cbb87d73105
Pipin Antosin <77954264+pipinantosin@users.noreply.github.com>
2026-02-24 15:13:28 +0700

    Update js/balance.js via push-zip tool

--- js/balance.js
+++ js/balance.js
@@ -200,3 +200,11 @@ function adjustBottomSpacing() {
 
 window.addEventListener("load", adjustBottomSpacing);
 window.addEventListener("resize", adjustBottomSpacing);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js")
+      .then(reg => console.log("SW registered"))
+      .catch(err => console.log("SW failed", err));
+  });
+}
